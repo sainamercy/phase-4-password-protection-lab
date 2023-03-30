9fc769e9941d952d8852ed05b4710f468c1d78d5
sainamercy <sainamercy@gmail.com>
2023-03-30 12:33:27 +0300

    done

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+    def create
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+        else
+            render json: {error: {login: "Invalid username or password"}}
+        end
+    end
+
+    def destroy
+        session.delete :user_id
+        head :no_content
+    end
+end
